706d97d99f79dcd75cb4c20a7d640613de163399
Md. Akhtar Uzzaman <akhtaruzzaman05@hotmail.com>
2021-11-09 17:35:12 +0600

    upd

---
+++ WebSocket/Models/TestB2Model.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebSocket.Models
+{
+    public class TestB2Model : IDisposable
+    {
+        public int MyPropertyTestB2 { get; set; }
+        public TestB2Model(int sl)
+        {
+            this.MyPropertyTestB2 = sl;
+        }
+        public int Sqrt()
+        {
+            return MyPropertyTestB2 * MyPropertyTestB2;
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
